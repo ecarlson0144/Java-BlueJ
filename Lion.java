f567811f2c569a80eb473a9e78ea0b65c947f974
Eric Carlson <ecarlson0144@myduneland.org>
2019-09-20 12:15:54 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Lion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lion
+{
+	private int a,age;
+
+	public void roar()
+	{
+		System.out.println("Roar!  Roar!");
+	} // roar() end
+	public void setAge(int a)
+	{
+	    age=(int) a;
+	}
+	public int getAge()
+	{
+	    return age;
+	    
+	 }   
+} //Lion end
---
+++ ZooProgram.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class ZooProgram here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ZooProgram {
+        public static void main (String [] args) {
+        Lion leo = new Lion ();
+        leo.roar();
+        leo.setAge(5);
+        System.out.println("Leo’s age is: " + leo.getAge());
+        
+        
+        
+    } // main () end
+} // ZooProgram end
